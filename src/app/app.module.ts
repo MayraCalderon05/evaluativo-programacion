37d37cef0e2f0a4c463381c497a1aef9c49ffd53
Mayra_Calderon <may2005_cc@hotmail.com>
2024-06-19 10:51:33 -0300

    Realicé la instalación de bootstrap, angular material y firebase; desde el angular.json configuré los estilos de boostrap. Después creé los archvos de vinculación con la base de datos y los configuré, por último, importé sus módulos en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Herramientas de la base de datos
+import { environment } from 'src/environments/environment'; //Vinculación de la base de datos con la app
+import { AngularFireModule } from '@angular/fire/compat'; //Importación e las colecciones de información
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'; //Imágenes y archivos
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //Importaciones de la base de datos
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+//Configuración de firebase en este proyecto de manera local
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyClGIF3WUJRDwiBMSgT8IKU7tWCk94a3zo",
+        authDomain: "calderon-database.firebaseapp.com",
+        projectId: "calderon-database",
+        storageBucket: "calderon-database.appspot.com",
+        messagingSenderId: "106140007792",
+        appId: "1:106140007792:web:c4f7463cd9088b690723b5"
+    }
+}
