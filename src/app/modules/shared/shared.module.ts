3d68cd31318c239a0606bcf7f4938fd65cc39001
Mayra_Calderon <may2005_cc@hotmail.com>
2024-06-28 08:57:24 -0300

    Empecé a crear el header de la página e idee la vista de visitante

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+//importaciones de angular
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 @NgModule({
@@ -11,7 +16,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
