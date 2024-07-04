9e5eb000989f7a41a00c7be13108c0bedd543834
Mayra_Calderon <may2005_cc@hotmail.com>
2024-07-04 08:31:58 -0300

    Configuré las reglas de las interfaces

--- src/app/models/elementos.ts
+++ src/app/models/elementos.ts
@@ -1,4 +1,10 @@
 export interface Elementos {
     uid:any;
-    
+    nombre:string;
+    cantidadPorPaquete:number;
+    sabor:string;
+    precio:number;
+    destacado:boolean;
+    imagenUrl:string;
+    favorito:boolean;
 }
--- src/app/models/libros.ts
+++ src/app/models/libros.ts
@@ -1,2 +1,10 @@
 export interface Libros {
+    uid:any;
+    titulo:string;
+    descripcion:string;
+    autor:string;
+    precio:number;
+    destacado:boolean;
+    imagenUrl:string;
+    favorito:boolean;
 }
