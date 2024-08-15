d3ef67b65c6d1e6844045763c785eb8803cdb7d0
Mayra_Calderon <may2005_cc@hotmail.com>
2024-08-15 08:44:52 -0300

    corrección de la estructura del modulo 'productos' para reestructurarla ya que estaba implementando mal un componente

--- src/app/modules/productos/productos-routing.module.ts
+++ src/app/modules/productos/productos-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductoComponent } from './components/producto/producto.component';
 
 const routes: Routes = [
-  {path:"producto",component:ProductoComponent}
+  
 ];
 
 @NgModule({
--- src/app/modules/productos/productos.module.ts
+++ src/app/modules/productos/productos.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductosRoutingModule } from './productos-routing.module';
-import { ProductoComponent } from './components/producto/producto.component';
 
 
 @NgModule({
   declarations: [
-    ProductoComponent
+
   ],
   imports: [
     CommonModule,
     ProductosRoutingModule
   ],
   exports:[
-    ProductoComponent
+  
   ]
 })
 export class ProductosModule { }
